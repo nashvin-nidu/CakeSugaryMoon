34f57241b204a7123a947eb4678a429b1ed70c7b
Nashvin Nidu <nashvinnidu1111@gmail.com>
2024-07-11 22:22:52 +0530

    Added Code

---
+++ script.js
@@ -0,0 +1,8 @@
+// This is an example of how to add click functionality to the button
+// You can add more Javascript to implement other features
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', function() {
+//   alert('You clicked the reserve button!');
+// });
